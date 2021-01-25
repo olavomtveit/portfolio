a8eac08a4fc3270ac9d0cb60d059c5e17c525775
olavomtveit <56303134+olavomtveit@users.noreply.github.com>
2021-01-25 08:38:07 +0100

    portfolio update 1

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const socialContainer = document.getElementById("socialIcon");
+const socialBtn = socialContainer.addEventListener("click", openSocial);
+const closeContainer = document.getElementById("closeIcon");
+const closeBtn = closeContainer.addEventListener("click", closeSocial);
+
+function openSocial () {
+    socialContainer.style.display = "none";
+    closeContainer.style.display = "flex";
+}
+
+function closeSocial () {
+    socialContainer.style.display = "flex";
+    closeContainer.style.display = "none";
+}
+
+const spaceXTarget = document.getElementById("spaceX-details");
+const shadowballTarget = document.getElementById("shadowball-details");
+const framxTarget = document.getElementById("framX-details");
+
+function showSpaceX () {
+    if (spaceXTarget.style.display === "" || spaceXTarget.style.display === "none") {
+        spaceXTarget.style.display = "block";
+    } else {
+        spaceXTarget.style.display = "none";
+    }
+}
+
+function showShadowball () {
+    if (shadowballTarget.style.display === "" || shadowballTarget.style.display === "none") {
+        shadowballTarget.style.display = "block";
+    } else {
+        shadowballTarget.style.display = "none";
+    }
+}
+
+function showFramX () {
+    if (framxTarget.style.display === "" || framxTarget.style.display === "none") {
+        framxTarget.style.display = "block";
+    } else {
+        framxTarget.style.display = "none";
+    }
+}
